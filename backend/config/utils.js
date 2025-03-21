55a795009fbfeeb506839741cdfbe1aece8a4f3a
sang24102000 <chisang24102000@gmail.com>
2025-03-22 04:59:51 +0700

    add file

--- backend/config/utils.js
+++ backend/config/utils.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI ="mongodb://127.0.0.1/wanderlust";
-const REDIS_URL = "127.0.0.1:6379";
+const MONGODB_URI =process.env.MONGODB_URI;
+const REDIS_URL = process.env.REDIS_URL;
 
 export { MONGODB_URI, PORT, REDIS_URL };
