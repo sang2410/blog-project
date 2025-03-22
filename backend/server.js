0331f8b767f00b7343247549416e0c8fd7803664
sang24102000 <chisang24102000@gmail.com>
2025-03-22 13:13:27 +0700

    add file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import authRouter from './routes/auth.js';
 import postsRouter from './routes/posts.js';
 import { connectToRedis } from './services/redis.js';
 const app = express();
-const port = PORT || 5000;
+const port = PORT ;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
